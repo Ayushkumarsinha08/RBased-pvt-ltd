028789aad31d42ef4c636a17eed20f960d346812
gulshank0 <gulshanbahadur002@gmail.com>
2025-05-20 00:40:06 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 
 import Content from "@/components/ui/firstpage-content"
 import { Navbar } from "@/components/ui/NavBar"
-import { div } from "framer-motion/client"
 
 export default function Home (){
     return (
