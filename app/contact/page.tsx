9a74e8a5fc034b8ce24497e904df2e534d542d7e
gulshank0 <gulshanbahadur002@gmail.com>
2025-05-21 14:14:23 +0530

    contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,27 @@
+import Footer from "@/components/ui/Footer";
 import { Navbar } from "@/components/ui/NavBar";
 
-export default function About(){
-    return(
-        <div>
-            <Navbar/>
+export default function Contact() {
+  return (
+    <div className="min-h-screen bg-black text-gray-100">
+      <Navbar />
+      <div className="container mx-auto py-12 px-4">
+        <h1 className="text-3xl font-bold text-center pb-8">Contact Us</h1>
+        
+        <div className="bg-white rounded-lg shadow-md p-6 max-w-3xl mx-auto">
+          <div className="w-full h-[90vh]">
+            <iframe 
+              src={process.env.GOOGLE_FORM_URL} // Replace with your Google Form URL
+              className="w-full h-full"
+              title="Contact Form"
+            >
+              Loading...
+            </iframe>
+          </div>
         </div>
-    )
+      </div>
+      <hr className="w-full border-white/20 " />
+      <Footer/>
+    </div>
+  );
 }
