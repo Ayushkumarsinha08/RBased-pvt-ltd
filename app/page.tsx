1212294ea4a82864046824e949c535493f5e3e5f
gulshank0 <gulshanbahadur002@gmail.com>
2025-07-10 20:34:47 +0530

    ui improved

--- app/page.tsx
+++ app/page.tsx
@@ -29,9 +29,9 @@ export default function Home ( ){
 
 
                 {/* Our Projects Section Header */}
-                <div className="pb-8 pt-8 text-center text-6xl font-extralight">OUR PROJECTS</div>               
+                <div className="pt-20 text-center text-6xl font-extralight">OUR PROJECTS</div>               
                 {/* Third Section - Card with video on left, Text on right */}
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 p-4">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 p-30">
                     <Card 
                         src={"/images/research-img.jpg"}
                         videoSrc={"/map-video.mp4"}
@@ -49,11 +49,11 @@ mapping, real-time overlays, and user-friendly dashboards.
                                   </div>
                 </div>
                 {/* Fourth Section - Text on left, Card on right */}
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 p-4 pt-8">  
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 p-30 pt-8">  
                     <div className="flex flex-col items-center pt-20 space-y-4">
                         <h2 className="text-4xl font-extralight md:text-5xl lg:text-6xl text-center">West Bengal Digital Info System</h2>
                         <p className="text-xl md:text-2xl lg:text-2xl font-extralight px-6 pt-4 leading-relaxed">
-                        The Digital Information System (DIS) for Industrial Estates in WestBengal is a web-based platform designed to streamline and bring transparency to the industrial plot allocation process. It introduces a bidding mechanism that promotes fair competition and ease of access. The system focuses on enhancing user interaction, enabling plot-level bidding, and improving performance, usability, and stakeholder engagement across the platform.
+                        The Digital Information System (DIS) for Industrial Estates in WestBengal is a web-based platform designed to streamline and bring transparency to the industrial plot allocation process. It introduces a bidding mechanism that promotes fair competition and ease of access. 
                         </p>
                                    </div>
                     <div className="space-y-4">
